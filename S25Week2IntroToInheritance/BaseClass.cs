7e545a187904b6832934fb839f5206cfb0acfc3e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-22 14:26:28 -0400

    protected

---
+++ S25Week2IntroToInheritance/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S25Week2IntroToInheritance
+{
+    public class BaseClass
+    {
+        private int myPrivateVar;
+        protected int myProtectedVar;
+        public int myPublicVar;
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public void MyMethod()
+        {
+            //myPrivateVar = 1;
+            myProtectedVar = 2;
+            myPublicVar = 3;
+        }
+    }
+}
--- S25Week2IntroToInheritance/Program.cs
+++ S25Week2IntroToInheritance/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DerivedClass objDerived = new DerivedClass();
+            objDerived.myPublicVar = 1;
         }
     }
 }
