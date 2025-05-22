4d8cdbe9aac29855c79901ebda8482dc831f3d94
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-22 15:21:11 -0400

    complete

--- S25Week2IntroToInheritance/Program.cs
+++ S25Week2IntroToInheritance/Program.cs
@@ -9,7 +9,16 @@
 
 
             //BaseClass objBase = new BaseClass();
-            DerivedClass objDerived = new DerivedClass();
+            //DerivedClass objDerived = new DerivedClass();
+
+
+            CommissionEmployee commEmp = new CommissionEmployee(101, "John", 10000, 0.15);
+            Console.WriteLine(commEmp);
+            Console.WriteLine($"\nEarnings = {commEmp.Earnings():C}\n\n");
+
+            SalaryPlusCommissionEmployee salCommEmp = new SalaryPlusCommissionEmployee(102, "Anne", 8000, 0.12, 500);
+            Console.WriteLine(salCommEmp);
+            Console.WriteLine($"\nEarnings = {salCommEmp.Earnings():C}\n\n");
         }
     }
 }
